d78d7292eb0f7ebbd97ca98284086ed4b471902f
Zak Thompson <stump@pop-os.localdomain>
2021-12-16 11:30:53 -0500

    Add templates and Discogs search

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from flask import Flask
+import json
+import os
+
+from flask import Flask, render_template, request
+import requests
+
+
+USER_AGENT = "SameVibe/0.1 +https://samevi.be"
+MUSIC_KEY = os.environ.get("DISCOGS_CONSUMER_KEY")
+MUSIC_SECRET = os.environ.get("DISCOGS_CONSUMER_SECRET")
+
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
-    return '<p>Hello, Butts!</p>'
+    return render_template("hello.html", key=MUSIC_KEY, secret=MUSIC_SECRET)
+
+
+@app.route("/search")
+def search():
+    q = request.args.get("q")
+    url = f"https://api.discogs.com/database/search?q={q}&key={MUSIC_KEY}&secret={MUSIC_SECRET}"
+    headers = {
+        "User-Agent": USER_AGENT,
+    }
+    response = requests.get(url, headers=headers).content
+    results = json.loads(response).get("results")
+    return render_template("search.html", search=q, results=results)
