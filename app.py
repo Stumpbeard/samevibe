49a283133410af52ae371181faaac5a0e29a77d7
Zak Thompson <stump@pop-os.localdomain>
2021-12-17 11:09:43 -0500

    Add release page

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ import requests
 USER_AGENT = "SameVibe/0.1 +https://samevi.be"
 MUSIC_KEY = os.environ.get("DISCOGS_CONSUMER_KEY")
 MUSIC_SECRET = os.environ.get("DISCOGS_CONSUMER_SECRET")
+DISCOGS_API = "https://api.discogs.com"
+HEADERS = {
+    "User-Agent": USER_AGENT,
+}
 
 
 app = Flask(__name__)
@@ -21,10 +25,27 @@ def hello_world():
 @app.route("/search")
 def search():
     q = request.args.get("q")
-    url = f"https://api.discogs.com/database/search?q={q}&key={MUSIC_KEY}&secret={MUSIC_SECRET}"
-    headers = {
-        "User-Agent": USER_AGENT,
-    }
-    response = requests.get(url, headers=headers).content
+    url = f"{DISCOGS_API}/database/search?q={q}&type=release&key={MUSIC_KEY}&secret={MUSIC_SECRET}"
+    response = requests.get(url, headers=HEADERS).content
     results = json.loads(response).get("results")
-    return render_template("search.html", search=q, results=results)
+    return render_template("search.html", search=q, results=results[:10])
+
+
+@app.route("/release/<id>")
+def music_release(id):
+    url = f"{DISCOGS_API}/releases/{id}?key={MUSIC_KEY}&secret={MUSIC_SECRET}"
+    response = requests.get(url, headers=HEADERS).content
+    results = json.loads(response)
+    print(results)
+    artist = results.get("artists", [None])[0].get("name")
+    title = results.get("title")
+    tracklist = results.get("tracklist", [])
+    image_url = results.get("images", [None])[0].get("resource_url")
+    print("image_url", image_url)
+    return render_template(
+        "artist-release.html",
+        artist=artist,
+        title=title,
+        tracklist=tracklist,
+        image_url=image_url,
+    )
